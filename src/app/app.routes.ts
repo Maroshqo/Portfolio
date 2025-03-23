c6420863258a786def8e05162c1475b0e4235079
Portfolio <maroshqo@gmail.com>
2025-03-23 01:12:24 +0100

    First commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {ProjectsComponent} from './pages/projects/projects.component';
+import {AiToolsComponent} from './pages/ai-tools/ai-tools.component';
+import {ContactComponent} from './pages/contact/contact.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'ai-tools', component: AiToolsComponent },
+  { path: 'contact', component: ContactComponent },
+];
