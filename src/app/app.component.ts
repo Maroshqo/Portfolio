671e2b5f944242abe7f437c0713e798e546ec0d7
Portfolio <maroshqo@gmail.com>
2025-05-02 03:45:53 +0200

    Click outside to close burger menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ClickOutsideDirective } from './shared/click-outside.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule, ClickOutsideDirective],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
